da595e557e76592182c3c082000e5101882a0895
CarolinaR99 <carolina_rocha3@hotmail.com>
2022-07-27 20:16:02 +0100

    fcc interv: iterate 1st arg

--- fcc/interview.js
+++ fcc/interview.js
@@ -1,7 +1,15 @@
 function sym(args) {
-  return args;
+  answer = [];
+  for (let i = 0; i < arguments.length - 1; i++) {
+    for (let j = 0; j < arguments[i].length; j++) {
+      if (arguments[i+1].includes(arguments[i][j]) == false) {
+        answer.push(arguments[i][j]);
+      }
+    }
+  }
+  return answer;
 }
 
 console.log(sym([1, 2, 3], [5, 2, 1, 4]));
 
-console.log(3);
+console.log(1);
